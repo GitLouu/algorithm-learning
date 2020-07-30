e52dc535cf282bdfdb4488fccb5ec7ed7689fd3d
mxm <1030647932@qq.com>
2020-07-30 23:07:22 +0800

    爬楼梯问题两种解法

---
+++ thinking-in-algorithm/src/main/java/com/gitee/fsmxm/thinking/in/algorithm/leetcode/_70_ClimbingStairs_Loop.java
@@ -0,0 +1,43 @@
+package com.gitee.fsmxm.thinking.in.algorithm.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 爬楼梯问题
+ * https://leetcode-cn.com/problems/climbing-stairs/
+ */
+public class _70_ClimbingStairs_Loop {
+
+    // 使用循环解决 （斐波那契数列问题）
+    static class Solution {
+
+        public int climbStairs(int n) {
+            if (n == 1) {
+                return 1;
+            }
+            if (n == 2) {
+                return 2;
+            }
+
+            int sum = 0;
+            for (int i = 3, step1 = 1, step2 = 2; i <= n; i++) {
+                sum = step1 + step2;
+                step1 = step2;
+                step2 = sum;
+            }
+            return sum;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.climbStairs(1));
+        System.out.println(solution.climbStairs(2));
+        System.out.println(solution.climbStairs(3));
+        System.out.println(solution.climbStairs(4));
+        System.out.println(solution.climbStairs(5));
+    }
+
+}
---
+++ thinking-in-algorithm/src/main/java/com/gitee/fsmxm/thinking/in/algorithm/leetcode/_70_ClimbingStairs_Recursion.java
@@ -0,0 +1,47 @@
+package com.gitee.fsmxm.thinking.in.algorithm.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 爬楼梯问题
+ * https://leetcode-cn.com/problems/climbing-stairs/
+ */
+public class _70_ClimbingStairs_Recursion {
+
+    // 使用递归解决
+    static class Solution {
+
+        // 记录计算结果，避免重复计算
+        Map<Integer, Integer> record = new HashMap<Integer, Integer>();
+
+        public int climbStairs(int n) {
+            if (n == 1) {
+                return 1;
+            }
+
+            if (n == 2) {
+                return 2;
+            }
+
+            if (record.containsKey(n)) {
+                return record.get(n);
+            }
+
+            int sum = climbStairs(n - 1) + climbStairs(n - 2);
+            record.put(n, sum);
+            return sum;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.climbStairs(1));
+        System.out.println(solution.climbStairs(2));
+        System.out.println(solution.climbStairs(3));
+        System.out.println(solution.climbStairs(4));
+        System.out.println(solution.climbStairs(5));
+    }
+
+}
