332201352ad05be9678722afcb5bdce24f8f6aca
mxm <1030647932@qq.com>
2020-08-17 14:59:25 +0800

    add tree node

---
+++ thinking-in-algorithm/src/main/java/com/gitee/fsmxm/thinking/in/algorithm/TNode.java
@@ -0,0 +1,18 @@
+package com.gitee.fsmxm.thinking.in.algorithm;
+
+/**
+ * tree node
+ */
+public class TNode<T> {
+
+    public T val;
+
+    public TNode<T> left;
+
+    public TNode<T> right;
+
+    public TNode(T val) {
+        this.val = val;
+    }
+
+}
