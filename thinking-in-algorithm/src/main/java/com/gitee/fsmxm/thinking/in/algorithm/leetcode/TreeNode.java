6b89b059601cb6549dbf3d6e049eb99cfdb0d0c4
mxm <1030647932@qq.com>
2020-08-17 18:11:50 +0800

    add

---
+++ thinking-in-algorithm/src/main/java/com/gitee/fsmxm/thinking/in/algorithm/leetcode/TreeNode.java
@@ -0,0 +1,11 @@
+package com.gitee.fsmxm.thinking.in.algorithm.leetcode;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
