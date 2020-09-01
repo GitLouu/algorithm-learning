1c7e38ff667a448350de7ea4313622321ede73bd
mxm <1030647932@qq.com>
2020-09-02 00:12:27 +0800

    leetcode 290

---
+++ thinking-in-algorithm/src/main/java/com/gitee/fsmxm/thinking/in/algorithm/leetcode/_290_WordPattern_Loop_Array_Set.java
@@ -0,0 +1,50 @@
+package com.gitee.fsmxm.thinking.in.algorithm.leetcode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 290. 单词规律
+ * https://leetcode-cn.com/problems/word-pattern/
+ * 难度 简单
+ */
+public class _290_WordPattern_Loop_Array_Set {
+    // T(n) = O(n)
+    static class Solution {
+        public boolean wordPattern(String pattern, String str) {
+            String[] ss = str.split(" ");
+            if (pattern.length() != ss.length) {
+                return false;
+            }
+            String[] pt = new String[26];
+            for (int i = 0; i < ss.length; i++) {
+                char c = pattern.charAt(i);
+                if (pt[c - 'a'] == null) {
+                    pt[c - 'a'] = ss[i];
+                } else {
+                    if (!pt[c - 'a'].equals(ss[i])) {
+                        return false;
+                    }
+                }
+            }
+            Set<String> set = new HashSet<>();
+            for (String s : pt) {
+                if (s != null) {
+                    if (set.contains(s)) {
+                        return false;
+                    }
+                    set.add(s);
+                }
+            }
+            return true;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.wordPattern("abba", "dog cat cat dog"));
+        System.out.println(solution.wordPattern("abba", "dog cat cat fish"));
+        System.out.println(solution.wordPattern("aaaa", "dog cat cat dog"));
+        System.out.println(solution.wordPattern("abba", "dog dog dog dog"));
+    }
+}
