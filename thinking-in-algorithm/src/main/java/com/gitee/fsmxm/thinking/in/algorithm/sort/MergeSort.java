96173f84c31a44da246f752472c38b52bb55ab2e
L <1030647932@qq.com>
2023-04-01 16:48:51 +0800

    remove unnecessary method

--- thinking-in-algorithm/src/main/java/com/gitee/fsmxm/thinking/in/algorithm/sort/MergeSort.java
+++ thinking-in-algorithm/src/main/java/com/gitee/fsmxm/thinking/in/algorithm/sort/MergeSort.java
@@ -1,7 +1,5 @@
 package com.gitee.fsmxm.thinking.in.algorithm.sort;
 
-import java.util.Arrays;
-
 /**
  * 归并排序
  */
@@ -15,29 +13,29 @@ public class MergeSort {
         sortInternally(data, 0, data.length - 1);
     }
 
-    private static void sortInternally(int[] data, int q, int r) {
+    private static void sortInternally(int[] data, int left, int right) {
 
         // 终止条件
-        if (q >= r) {
+        if (left >= right) {
             return;
         }
 
-        int mid = (q + r) / 2;
-        sortInternally(data, q, mid);
-        sortInternally(data, mid + 1, r);
+        int mid = (left + right) / 2;
+        sortInternally(data, left, mid);
+        sortInternally(data, mid + 1, right);
 
-        merge(data, q, mid, r);
+        merge(data, left, mid, right);
 
     }
 
-    private static void merge(int[] data, int q, int mid, int r) {
+    private static void merge(int[] data, int left, int mid, int right) {
         // 申请q...r个空间的数组
-        int[] temp = new int[r + 1 - q];
-        int i = q;
+        int[] temp = new int[right + 1 - left];
+        int i = left;
         int j = mid + 1;
         int pos = 0;
         // 将数据有序的放入临时数组
-        while (i <= mid && j <= r) {
+        while (i <= mid && j <= right) {
             if (data[i] > data[j]) { // 保证数据稳定性（相同数值数据前后位置不变）
                 temp[pos++] = data[j];
                 j++;
@@ -53,21 +51,15 @@ public class MergeSort {
         }
 
         // 如果有剩余数据，将剩余数据压入数组
-        while (j <= r) {
+        while (j <= right) {
             temp[pos++] = data[j++];
         }
 
         // 将临时数组数据复制给原数组
         for (i = 0; i < temp.length; i++) {
-            data[i + q] = temp[i];
+            data[i + left] = temp[i];
         }
 
-    }
-
-    public static void main(String[] args) {
-        int[] a = {1, 3, 6, 5, 9, 8, 0, 2, 7, 4};
-        sort(a);
-        System.out.println(Arrays.toString(a));
     }
 
 }
