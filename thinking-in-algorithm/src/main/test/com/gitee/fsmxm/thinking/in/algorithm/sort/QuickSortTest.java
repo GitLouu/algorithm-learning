01a589e656b31039cbfc671578d8e2cef82b7c41
L <1030647932@qq.com>
2023-04-01 16:42:05 +0800

    add

---
+++ thinking-in-algorithm/src/main/test/com/gitee/fsmxm/thinking/in/algorithm/sort/QuickSortTest.java
@@ -0,0 +1,40 @@
+package com.gitee.fsmxm.thinking.in.algorithm.sort;
+
+import com.gitee.fsmxm.thinking.in.algorithm.util.RandomUtil;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class QuickSortTest {
+    int[] origins = RandomUtil.create(20);
+
+    @Test
+    void sort1() {
+        for (int i = 0; i < 50; i++) {
+            System.out.println("原始: " + Arrays.toString(origins));
+            int[] arr = RandomUtil.copyAndShuffle(origins);
+            System.out.println("打乱: " + Arrays.toString(arr));
+            assertFalse(Arrays.equals(arr, origins)); // 如果数组长度太小,比如3,那么这里是有可能相等的,因为随机的结果集变小了
+            QuickSort.sort1(arr);
+            assertArrayEquals(arr, origins);
+            System.out.println("排序: " + Arrays.toString(arr));
+            System.out.println("---");
+        }
+    }
+
+    @Test
+    void sort2() {
+        for (int i = 0; i < 50; i++) {
+            System.out.println("原始: " + Arrays.toString(origins));
+            int[] arr = RandomUtil.copyAndShuffle(origins);
+            System.out.println("打乱: " + Arrays.toString(arr));
+            assertFalse(Arrays.equals(arr, origins)); // 如果数组长度太小,比如3,那么这里是有可能相等的,因为随机的结果集变小了
+            QuickSort.sort2(arr);
+            assertArrayEquals(arr, origins);
+            System.out.println("排序: " + Arrays.toString(arr));
+            System.out.println("---");
+        }
+    }
+}
