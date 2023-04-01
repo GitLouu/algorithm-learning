16db910244081d601a917066814b77b278b1aa17
L <1030647932@qq.com>
2023-04-01 16:48:58 +0800

    add

---
+++ thinking-in-algorithm/src/main/test/com/gitee/fsmxm/thinking/in/algorithm/sort/MergeSortTest.java
@@ -0,0 +1,27 @@
+package com.gitee.fsmxm.thinking.in.algorithm.sort;
+
+import com.gitee.fsmxm.thinking.in.algorithm.util.RandomUtil;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MergeSortTest {
+
+    int[] origins = RandomUtil.create(20);
+
+    @Test
+    void sort() {
+        for (int i = 0; i < 50; i++) {
+            System.out.println("原始: " + Arrays.toString(origins));
+            int[] arr = RandomUtil.copyAndShuffle(origins);
+            System.out.println("打乱: " + Arrays.toString(arr));
+            assertFalse(Arrays.equals(arr, origins)); // 如果数组长度太小,比如3,那么这里是有可能相等的,因为随机的结果集变小了
+            MergeSort.sort(arr);
+            assertArrayEquals(arr, origins);
+            System.out.println("排序: " + Arrays.toString(arr));
+            System.out.println("---");
+        }
+    }
+}
